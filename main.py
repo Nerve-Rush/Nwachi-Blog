3e6ba884574ec3cc67da730d84e5531435474789
Chioma Mbaka <chioma.mbaka@yahoo.com>
2022-06-04 01:38:57 +0100

    Environment variable change

--- main.py
+++ main.py
@@ -10,15 +10,19 @@ from sqlalchemy.ext.declarative import declarative_base
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
 from forms import CreatePostForm, UserForm, LoginForm, CommentForm
 from flask_gravatar import Gravatar
+import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+os.environ['SQLALCHEMY_DATABASE_URL'] = 'sqlite:///blog.db'
+os.environ["SECRET_KEY"] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 ckeditor = CKEditor(app)
 Base = declarative_base()
 Bootstrap(app)
+# print(os.environ.get("SECRET_KEY"))
 
 ##CONNECT TO DB
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 gravatar = Gravatar(app=app)
